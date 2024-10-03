5478ee2611e2a00f6f05a692ea30f7fe3cc1dd35
chestermalubayyy <chestermalubay@gmail.com>
2024-10-03 10:46:34 +0800

    Add files via upload

---
+++ 03VariableDeclaration.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+
+{
+	int age;
+	
+	
+	cout << "Enter Your Age\t\t:\t";
+	cin >> age;
+	cout << "Thank You!" << endl;
+	
+	return 0;
+	
+}
