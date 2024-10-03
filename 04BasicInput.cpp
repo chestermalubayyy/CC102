f6368473844a51a8146705a07ffda8f20a34f44e
chestermalubayyy <chestermalubay@gmail.com>
2024-10-03 11:33:45 +0800

    Add files via upload

---
+++ 04BasicInput.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+	string nn, CL, school;
+	
+	cout << "Enter Nickname\t\t:\t";
+	cin >> nn;
+	cout << "Course-Year Level\t:\t";
+	cin >> CL;
+	cout << "Enter School\t\t:\t";
+	cin >> school;
+	cout << "Wow Congrats " << nn << " is a nice course and you are studying in OLLCF which is one of the Center of Excellence in Tertiary Education" << endl;
+
+
+	return 0;
+}
