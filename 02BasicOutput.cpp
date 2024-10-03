3f469f91df5684d3ba8e962c514f9e76d8b9da10
chestermalubayyy <chestermalubay@gmail.com>
2024-10-03 10:40:17 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+	 cout << "Our Lady Of Lourdes College Foundation" << endl;
+	 cout << "Computer Science Technology" << endl;
+	 cout << "Bachelor Science in Information Technology" << endl;
+	 cout << "Chester Floyd M. Malubay" << endl;
+	 cout << "P-1, Brgy. Gumamela, Labo Camarines Norte" << endl;
+	 return 0;
+	 
+}
